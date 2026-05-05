762dc64255d5cc291c1a602ad43f0873e78620d3
Chris <akikichriss@gmail.com>
2026-05-05 11:03:26 +0300

    feat: add support for dine-in URL scheme in AndroidManifest and update socket URL in ScanTableScreen

--- src/screens/ScanTableScreen.tsx
+++ src/screens/ScanTableScreen.tsx
@@ -29,9 +29,9 @@ const ScanTableScreen = () => {
 
   // TEST: auto-set branchTable to bypass QR scanning
   useEffect(() => {
-    dispatch(setBranchTable('ashrafieh.table4'));
-    // dispatch(setSocketUrl('wss://crepaway-dinein-websocket-ts-hgga.onrender.com'));
-    dispatch(setSocketUrl('http://192.168.1.8:5003'));
+    // dispatch(setBranchTable('ashrafieh.table4'));
+    dispatch(setSocketUrl('wss://crepaway-dinein-websocket-ts-hgga.onrender.com'));
+    // dispatch(setSocketUrl('http://192.168.18.132:5003'));
   }, []);
 
   useFocusEffect(
